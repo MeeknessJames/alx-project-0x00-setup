28a3038062c9fec86ebb475a493432de06f997d6
Meekness James <meeknessjames1@gmail.com>
2025-07-29 05:49:07 +0100

    Task 5: Add PillProps interface and update Pill component to use props

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,11 +1,12 @@
-import React from 'react'; // Add this line for good practice
+import React from "react";
+import { PillProps } from "@/interfaces";
 
-const Pill: React.FC = () => {
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
-    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className="text-sm">{title}</p>
     </div>
-  )
-}
+  );
+};
 
 export default Pill;
